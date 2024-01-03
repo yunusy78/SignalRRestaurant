708ca8fd8f3a953c01805af765087f4847f62c3a
Yunus Yigit <yunusy@uia.no>
2024-01-04 00:36:44 +0100

    first commit

---
+++ Backend/WebApi/Controllers/AboutController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BusinessLayer.Abstract;
+using DtoLayer.AboutDtos;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+        private readonly IMapper _mapper;
+        
+        
+        public AboutController(IAboutService aboutService, IMapper mapper)
+        {
+            _aboutService = aboutService;
+            _mapper = mapper;
+        }
+        
+       
+        
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _aboutService.GetAllAsync();
+            return Ok(result);
+        }
+        
+        [HttpGet("{id}")]
+        
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _aboutService.GetByIdAsync(id);
+            return Ok(result);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Add(CreateAboutDto about)
+        {
+            var result = _mapper.Map<About>(about);
+            await _aboutService.AddAsync(result);
+            return Ok("Added Successfully");
+        }
+        
+        [HttpPut]
+        
+        public async Task<IActionResult> Update(UpdateAboutDto about)
+        {
+            var result = _mapper.Map<About>(about);
+            await _aboutService.UpdateAsync(result);
+            return Ok("Updated Successfully");
+        }
+        
+        [HttpDelete("{id}")]
+        
+        public async Task<IActionResult> Delete(int id)
+        {
+            var about = await _aboutService.GetByIdAsync(id);
+            await _aboutService.DeleteAsync(about);
+            return Ok("Deleted Successfully");
+        }
+        
+        
+        
+        
+    }
+}
---
+++ Backend/WebApi/Program.cs
@@ -0,0 +1,34 @@
+using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SignalRContext>();
+builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+builder.Services.AddScoped<IAboutService, AboutManager>();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
